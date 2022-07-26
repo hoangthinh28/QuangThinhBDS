045d148506f5de33b413748b8db2e6fad332d89b
hoangthinh28 <hoangthinhpro2001@gmail.com>
2022-07-26 17:16:18 +0700

    sql

--- sql/quangthinhbds_bookingtable.sql
+++ sql/quangthinhbds_bookingtable.sql
@@ -29,10 +29,14 @@ CREATE TABLE `bookingtable` (
   `Address` varchar(250) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
   `imgURL` varchar(250) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
   `Price` float DEFAULT NULL,
-  `DatesBooked` varchar(250) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
+  `Checkint` varchar(250) DEFAULT NULL,
+  `Checkout` varchar(250) DEFAULT NULL,
+  `RealEstateId` int DEFAULT NULL,
   `ethAddress` varchar(250) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
-  PRIMARY KEY (`BookingID`)
-) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`BookingID`),
+  UNIQUE KEY `Checkint` (`Checkint`),
+  UNIQUE KEY `Checkout` (`Checkout`)
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +45,7 @@ CREATE TABLE `bookingtable` (
 
 LOCK TABLES `bookingtable` WRITE;
 /*!40000 ALTER TABLE `bookingtable` DISABLE KEYS */;
+INSERT INTO `bookingtable` VALUES (92,'02 Bedroom Standard - Nội thất Đầy đủ - Vinhomes Ocean Park','S2.201109','Ho Chi Minh','https://stay.vinhomes.vn/on/demandware.static/-/Sites-vhm_leasing_en_master/vi_VN/dw912a868f/images/Vinhomes%20Ocean%20Park/2PN_Standard/2PN_Standard_5_large.jpg',45,'26/07/2022','29/07/2022',26,'0x90F79bf6EB2c4f870365E785982E1f101E93b906'),(93,'02 Bedroom Standard - Nội thất Đầy đủ - Vinhomes Ocean Park','S2.201109','Ho Chi Minh','https://stay.vinhomes.vn/on/demandware.static/-/Sites-vhm_leasing_en_master/vi_VN/dw912a868f/images/Vinhomes%20Ocean%20Park/2PN_Standard/2PN_Standard_5_large.jpg',30,'29/07/2022','31/07/2022',26,'0x90F79bf6EB2c4f870365E785982E1f101E93b906'),(97,'02 Bedroom Standard - Nội thất Đầy đủ - Vinhomes Ocean Park','S2.201109','Ho Chi Minh','https://stay.vinhomes.vn/on/demandware.static/-/Sites-vhm_leasing_en_master/vi_VN/dw912a868f/images/Vinhomes%20Ocean%20Park/2PN_Standard/2PN_Standard_5_large.jpg',30,'01/08/2022','03/08/2022',26,'0x90F79bf6EB2c4f870365E785982E1f101E93b906');
 /*!40000 ALTER TABLE `bookingtable` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-22 17:37:19
+-- Dump completed on 2022-07-26 17:15:05
